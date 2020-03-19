1f388b5b361aa01a33e41c9c196adc15f71311bc
Linxerz <pruittj2000@Yahoo.com>
2020-03-19 10:16:15 -0500

    Johnathan Pruitt

--- app/models/viewer.rb
+++ app/models/viewer.rb
@@ -8,8 +8,38 @@ class Viewer
     self.class.all << self
   end
 
+  def reviews
+    Review.all.select {|review| review.viewer == self}
+  end  
+
+  def reviewed_movies
+    self.reviews.map {|review| review.movie}
+  end
+
+  def reviewed_movie?(movie)
+    self.reviewed_movies.include?(movie)
+  end
+
+  def rate_movie(movie,rating)
+    if self.reviewed_movie?(movie)
+      Review.all.select{|review| review.movie == movie && review.viewer == self}[0].rating = rating
+    else
+      Review.new(self,movie,rating)
+    end
+  end
+
   def self.all
     @@all
   end
   
 end
+
+#   #### Viewer
+
+# - `Viewer#reviewed_movie?(movie)`
+#   - a `Movie` instance is the only argument
+#   - returns `true` if the `Viewer` has reviewed this `Movie` (if there is a `Review` instance that has this `Viewer` and `Movie`), returns `false` otherwise
+# - `Viewer#rate_movie(movie, rating)`
+#   - a `Movie` instance and a rating (number) are passed in as arguments
+#   - if the `Viewer` instance and the passed `Movie` instance are _not_ already associated, this method should create a new `Review` instance
+#   - if this `Viewer` has already reviewed this `Movie`, assigns the new rating to the existing `Review` instance
--- tools/console.rb
+++ tools/console.rb
@@ -11,8 +11,15 @@ end
 # so they will be available to test and play around with in your console
 
 
+gbh = Movie.new("The Grand Budhapest Hotel")
+linxerz = Viewer.new("Linxerz")
+gbh_review = Review.new(linxerz,gbh,10)
 
+mff = Movie.new("Mr.Fantastic Fox")
+mff_review = Review.new(linxerz,mff,10)
 
+felco = Viewer.new("Felco")
+gbh_review2 = Review.new(felco,gbh,9.5)
 
 # ===== WARNING! DO NOT EDIT BELOW THIS LINE ===== #
 binding.pry
